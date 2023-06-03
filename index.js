ca657564a82610024fe457c2104005e406f9c55f
Douglas M. Barcellos <douglasa321@gmail.com>
2023-06-03 12:43:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3030;
-const staticResponse = require("./response.json");
+const staticResponse = require('./response.json');
 
 const fetch = (...args) =>
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
